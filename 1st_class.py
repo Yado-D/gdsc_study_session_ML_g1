c6800a3f363bf48291e31cadae7bf7b8332ea7e7
Yado-D <yarachesu@gmail.com>
2025-11-30 15:09:27 -0500

    First class : create numpy array and calculate mean,sum,maximum

---
+++ 1st_class.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+sample_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sample_np_array = np.array(sample_array)
+
+print("mean : ",np.mean(sample_np_array))
+print("maximum value : ",np.max(sample_np_array))
+print("sum : ",np.sum(sample_np_array))
